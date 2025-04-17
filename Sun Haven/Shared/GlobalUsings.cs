5c1e2218a05426e7e6184b8c503ebf6e55daa3df
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2025-04-17 14:43:59 +1000

    SH Update

---
+++ Sun Haven/Shared/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿// Global using directives
+
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
+global using BepInEx.Configuration;
+global using PSS;
+global using QFSW.QC;
+global using UnityEngine;
+global using UnityEngine.UI;
+global using Wish;
+global using static BepInEx.Bootstrap.Chainloader;
